805e149b95f374d2fcd4c91ee95b7ffdfd72a9ac
odevincenti <odevincenti@itba.edu.ar>
2023-05-04 19:21:41 -0300

    Agrego cuentas PWM

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+C_values = np.array([150E-12, 180E-12, 270E-12, 560E-12, 820E-12, 1E-9, 1.5E-9, 2E-9, 2.2E-9, 2.7E-9, 3.3E-9, 4.7E-9, 5.6E-9, 6.8E-9, 8.2E-9, 10E-9, 12E-9, 22E-9, 33E-9, 39E-9, 47E-9, 100E-9, 150E-9, 220E-9, 330E-9, 1E-6])
+R_values = np.array([10.0, 100.0, 120.0, 150.0, 220.0, 330.0, 390.0, 560.0, 1E3, 1.2E3, 1.5E3, 1.8E3, 2.2E3, 2.7E3, 3E3, 3.3E3, 3.9E3, 4.7E3, 5.6E3, 6.2E3, 10E3, 12E3, 15E3, 20E3, 22E3, 27E3, 33E3, 39E3, 47E3, 56E3, 82E3, 100E3, 220E3, 300E3, 470E3, 820E3])
+
 def to_unit(num, sf=3):
 	"""Round a number to the specified significant figures (default is 3) and add the corresponding unit prefix.
 
@@ -54,3 +57,7 @@ def get_unit_prefix(num):
 		unit_prefix = prefix[e_count]
 
 	return unit_prefix
+
+def parallel(R1, R2):
+	R = 1/(1/R1 + 1/R2)
+	return R
